7598a2a5886c51861a593b93085c9957a8eec7b0
Linus Torvalds <torvalds@linux-foundation.org>
2011-05-04 21:30:28 -0700

    Resolve merge conflict for commit a1fde08c74e9

--- mm/gup.c
+++ mm/gup.c
@@ -483,10 +483,9 @@ long __get_user_pages(struct task_struct *tsk, struct mm_struct *mm,
 				return i ? : -EFAULT;
 
 		/*
-		 * If we don't actually want the page itself,
-		 * and it's the stack guard page, just skip it.
+		 * For mlock, just skip the stack guard page.
 		 */
-		if (!pages && stack_guard_page(vma, start))
+		if ((gup_flags & FOLL_MLOCK) && stack_guard_page(vma, start))
 			goto next_page;
 
 			if (is_vm_hugetlb_page(vma)) {
