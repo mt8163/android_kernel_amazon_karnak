1a01dcaf8a6fe28a5a3fc18364dd65cdf8566550
Kees Cook <keescook@chromium.org>
2014-04-03 17:28:11 -0700

    Resolve merge conflict for commit 1e6b48116a95

--- arch/arm/mm/mmu.c
+++ arch/arm/mm/mmu.c
@@ -1321,11 +1321,18 @@ static void __init map_lowmem(void)
 		if (start >= end)
 			continue;
 
-		if (end < kernel_x_start || start >= kernel_x_end) {
+		if (end < kernel_x_start) {
 			map.pfn = __phys_to_pfn(start);
 			map.virtual = __phys_to_virt(start);
 			map.length = end - start;
 			map.type = MT_MEMORY_RWX;
+
+			create_mapping(&map);
+		} else if (start >= kernel_x_end) {
+			map.pfn = __phys_to_pfn(start);
+			map.virtual = __phys_to_virt(start);
+			map.length = end - start;
+			map.type = MT_MEMORY_RW;
 
 			create_mapping(&map);
 		} else {
