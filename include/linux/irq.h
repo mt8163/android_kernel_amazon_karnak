f440b90fc3caf8be35356db0f01ae6866fd42d4f
Kevin Cernekee <cernekee@gmail.com>
2014-11-06 22:44:17 -0800

    Resolve merge conflict for commit b5937a545309

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -21,6 +21,7 @@
 #include <linux/errno.h>
 #include <linux/topology.h>
 #include <linux/wait.h>
+#include <linux/io.h>
 
 #include <asm/irq.h>
 #include <asm/ptrace.h>
@@ -660,13 +661,6 @@ void arch_teardown_hwirq(unsigned int irq);
 void irq_init_desc(unsigned int irq);
 #endif
 
-#ifndef irq_reg_writel
-# define irq_reg_writel(val, addr)	writel(val, addr)
-#endif
-#ifndef irq_reg_readl
-# define irq_reg_readl(addr)		readl(addr)
-#endif
-
 /**
  * struct irq_chip_regs - register offsets for struct irq_gci
  * @enable:	Enable register offset to reg_base
@@ -863,5 +857,17 @@ void dump_irq_need_migrate_list(const struct cpumask *mask);
 extern bool mt_get_irq_gic_targets(struct irq_data *d, cpumask_t *mask);
 extern bool mt_is_secure_irq(struct irq_data *d);
 #endif
+
+static inline void irq_reg_writel(struct irq_chip_generic *gc,
+				  u32 val, int reg_offset)
+{
+	writel(val, gc->reg_base + reg_offset);
+}
+
+static inline u32 irq_reg_readl(struct irq_chip_generic *gc,
+				int reg_offset)
+{
+	return readl(gc->reg_base + reg_offset);
+}
 
 #endif /* _LINUX_IRQ_H */
