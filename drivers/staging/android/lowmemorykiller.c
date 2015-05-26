431dc45c282891fbbba40f5ed6d271f2c9e03293
Martijn Coenen <maco@google.com>
2015-05-26 11:28:47 +0200

    lowmemorykiller: trace kill events.
    Allows for capturing lmk kill events and
    their rationale.
    
    Change-Id: Ibe215db5bb9806fc550c72c0b9832c85cbd56bf6
    Signed-off-by: Martijn Coenen <maco@google.com>
    
     Conflicts:
    	drivers/staging/android/lowmemorykiller.c

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -77,6 +77,9 @@ static uint32_t enable_candidate_log = 1;
 static uint32_t enable_candidate_log;
 #endif
 static DEFINE_SPINLOCK(lowmem_shrink_lock);
+#define CREATE_TRACE_POINTS
+#include "trace/lowmemorykiller.h"
+
 static uint32_t lowmem_debug_level = 1;
 static short lowmem_adj[9] = {
 	0,
@@ -506,20 +509,22 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 /* fosmod_fireos_crash_reporting end */
 
 	if (selected) {
-		lowmem_print(1, "Killing '%s' (%d), adj %d, score_adj %hd,\n"
-				"   to free %ldkB on behalf of '%s' (%d) because\n"
-				"   cache %ldkB is below limit %ldkB for oom_score_adj %hd\n"
+		long cache_size = other_file * (long)(PAGE_SIZE / 1024);
+		long cache_limit = minfree * (long)(PAGE_SIZE / 1024);
+		long free = other_free * (long)(PAGE_SIZE / 1024);
+		trace_lowmemory_kill(selected, cache_size, cache_limit, free);
+		lowmem_print(1, "Killing '%s' (%d), adj %hd,\n" \
+				"   to free %ldkB on behalf of '%s' (%d) because\n" \
+				"   cache %ldkB is below limit %ldkB for oom_score_adj %hd\n" \
 				"   Free memory is %ldkB above reserved\n",
 			     selected->comm, selected->pid,
 				 REVERT_ADJ(selected_oom_score_adj),
 			     selected_oom_score_adj,
 			     selected_tasksize * (long)(PAGE_SIZE / 1024),
 			     current->comm, current->pid,
-			     other_file * (long)(PAGE_SIZE / 1024),
-			     minfree * (long)(PAGE_SIZE / 1024),
+			     cache_size, cache_limit,
 			     min_score_adj,
-			     other_free * (long)(PAGE_SIZE / 1024));
-		lowmem_deathpending = selected;
+			     free);
 		lowmem_deathpending_timeout = jiffies + HZ;
 		set_tsk_thread_flag(selected, TIF_MEMDIE);
 
