f3af14340c299b2e5e60d43466d4835b7fee4a6b
Daniel Rosenberg <drosen@google.com>
2016-04-22 00:00:14 -0700

    vfs: change d_canonical_path to take two paths
    bug: 23904372
    Change-Id: I4a686d64b6de37decf60019be1718e1d820193e6
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    
     Conflicts:
    	fs/notify/inotify/inotify_user.c
    	include/linux/dcache.h
    
     It looks like you may be committing a cherry-pick.
     If this is not correct, please remove the file
    	.git/CHERRY_PICK_HEAD
     and try again.

--- fs/notify/inotify/inotify_user.c
+++ fs/notify/inotify/inotify_user.c
@@ -730,6 +730,15 @@ SYSCALL_DEFINE3(inotify_add_watch, int, fd, const char __user *, pathname,
 	if (ret)
 		goto fput_and_out;
 
+	/* support stacked filesystems */
+	if(path.dentry && path.dentry->d_op) {
+		if (path.dentry->d_op->d_canonical_path) {
+			path.dentry->d_op->d_canonical_path(&path, &alteredpath);
+			canonical_path = &alteredpath;
+			path_put(&path);
+		}
+	}
+
 	/* inode held in place by reference to path; group by fget on fd */
 	inode = path.dentry->d_inode;
 	group = f.file->private_data;
