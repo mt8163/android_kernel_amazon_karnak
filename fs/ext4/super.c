7318053f4e8665fedbd62163a3a9b55270844a89
Theodore Ts'o <tytso@mit.edu>
2016-07-05 20:01:52 -0400

    Resolve merge conflict for commit 5467159ce7da

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -3699,6 +3699,11 @@ static int ext4_fill_super(struct super_block *sb, void *data, int silent)
 	    es->s_encryption_level) {
 		ext4_msg(sb, KERN_ERR, "Unsupported encryption level %d",
 			 es->s_encryption_level);
+
+	if (le16_to_cpu(sbi->s_es->s_reserved_gdt_blocks) > (blocksize / 4)) {
+		ext4_msg(sb, KERN_ERR,
+			 "Number of reserved GDT blocks insanely large: %d",
+			 le16_to_cpu(sbi->s_es->s_reserved_gdt_blocks));
 		goto failed_mount;
 	}
 
