4e89617a4d09399f0ab266109e361eaa0ca72c5a
Boris Brezillon <boris.brezillon@free-electrons.com>
2016-09-13 15:58:28 +0200

    Resolve merge conflict for commit 790e65fd71e0

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -858,6 +858,16 @@ extern bool mt_get_irq_gic_targets(struct irq_data *d, cpumask_t *mask);
 extern bool mt_is_secure_irq(struct irq_data *d);
 #endif
 
+/*
+ * The irqsave variants are for usage in non interrupt code. Do not use
+ * them in irq_chip callbacks. Use irq_gc_lock() instead.
+ */
+#define irq_gc_lock_irqsave(gc, flags)	\
+	raw_spin_lock_irqsave(&(gc)->lock, flags)
+
+#define irq_gc_unlock_irqrestore(gc, flags)	\
+	raw_spin_unlock_irqrestore(&(gc)->lock, flags)
+
 static inline void irq_reg_writel(struct irq_chip_generic *gc,
 				  u32 val, int reg_offset)
 {
