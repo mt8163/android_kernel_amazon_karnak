0f0d77a5cd81c3aecd0cee4fb8735e62bd2e5862
Daniel Rosenberg <drosen@google.com>
2018-04-24 18:06:56 -0700

    ANDROID: sdcardfs: Don't d_drop in d_revalidate
    After d_revalidate returns 0, the vfs will call
    d_invalidate, which will call d_drop itself, along
    with other cleanup.
    
    Bug: 78262592
    Change-Id: Idbb30e008c05d62edf2217679cb6a5517d8d1a2c
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    
     Conflicts:
    	fs/sdcardfs/dentry.c

--- fs/sdcardfs/dentry.c
+++ fs/sdcardfs/dentry.c
@@ -51,7 +51,6 @@ static int sdcardfs_d_revalidate(struct dentry *dentry, unsigned int flags)
 	 * whether the base obbpath has been changed or not
 	 */
 	if (is_obbpath_invalid(dentry)) {
-		d_drop(dentry);
 		return 0;
 	}
 
@@ -65,7 +64,6 @@ static int sdcardfs_d_revalidate(struct dentry *dentry, unsigned int flags)
 	if ((lower_dentry->d_flags & DCACHE_OP_REVALIDATE)) {
 		err = lower_dentry->d_op->d_revalidate(lower_dentry, flags);
 		if (err == 0) {
-			d_drop(dentry);
 			goto out;
 		}
 	}
@@ -73,14 +71,12 @@ static int sdcardfs_d_revalidate(struct dentry *dentry, unsigned int flags)
 	spin_lock(&lower_dentry->d_lock);
 	if (d_unhashed(lower_dentry)) {
 		spin_unlock(&lower_dentry->d_lock);
-		d_drop(dentry);
 		err = 0;
 		goto out;
 	}
 	spin_unlock(&lower_dentry->d_lock);
 
 	if (parent_lower_dentry != lower_cur_parent_dentry) {
-		d_drop(dentry);
 		err = 0;
 		goto out;
 	}
@@ -94,7 +90,6 @@ static int sdcardfs_d_revalidate(struct dentry *dentry, unsigned int flags)
 	}
 
 	if (!qstr_case_eq(&dentry->d_name, &lower_dentry->d_name)) {
-		__d_drop(dentry);
 		err = 0;
 	}
 
@@ -112,8 +107,7 @@ static int sdcardfs_d_revalidate(struct dentry *dentry, unsigned int flags)
 	inode = dentry->d_inode;
 	if (inode) {
 		data = top_data_get(SDCARDFS_I(inode));
-		if (data->abandoned) {
-			d_drop(dentry);
+		if (!data || data->abandoned) {
 			err = 0;
 		}
 		data_put(data);
