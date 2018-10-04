31b99fa3529376fbd03ed33e433deb4377814199
Linus Torvalds <torvalds@linux-foundation.org>
2018-10-04 11:06:13 -0700

    tty: wipe buffer.
    commit c9a8e5fce009e3c601a43c49ea9dbcb25d1ffac5 upstream.
    
    After we are done with the tty buffer, zero it out.
    
    Reported-by: aszlig <aszlig@nix.build>
    Tested-by: Milan Broz <gmazyland@gmail.com>
    Tested-by: Daniel Zatovic <daniel.zatovic@gmail.com>
    Tested-by: aszlig <aszlig@nix.build>
    Cc: Willy Tarreau <w@1wt.eu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/tty/tty_buffer.c
+++ drivers/tty/tty_buffer.c
@@ -419,6 +419,9 @@ receive_buf(struct tty_struct *tty, struct tty_buffer *head, int count)
 		if (count)
 			disc->ops->receive_buf(tty, p, f, count);
 	}
+	head->read += count;
+	if (count > 0)
+		memset(p, 0, count);
 	return count;
 }
 
