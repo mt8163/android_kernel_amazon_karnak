cce1ac47e215278fd268919b99ca227f8db7fdc9
Kai Jones <zaksenoj@icloud.com>
2019-09-11 23:19:16 -0700

    fix duplicate definition for build

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -691,7 +691,6 @@ static void input_dev_release_keys(struct input_dev *dev)
 {
 	bool need_sync = false;
 	int code;
-	bool need_sync = false;
 
 	if (is_event_supported(EV_KEY, dev->evbit, EV_MAX)) {
 		for (code = 0; code <= KEY_MAX; code++) {
