1252f2181933423a56ba3d615d572c33e2ea3974
Kai Jones <zaksenoj@icloud.com>
2019-09-24 08:43:30 -0700

    Revert "alarmtimer: Add functions for timerfd support"
    This reverts commit 069e9d95e40734ab52b18fcd372fd66fc193243e.

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -361,31 +361,6 @@ void alarm_restart(struct alarm *alarm)
 EXPORT_SYMBOL_GPL(alarm_restart);
 
 /**
- * alarm_start_relative - Sets a relative alarm to fire
- * @alarm: ptr to alarm to set
- * @start: time relative to now to run the alarm
- */
-int alarm_start_relative(struct alarm *alarm, ktime_t start)
-{
-	struct alarm_base *base = &alarm_bases[alarm->type];
-
-	start = ktime_add(start, base->gettime());
-	return alarm_start(alarm, start);
-}
-
-void alarm_restart(struct alarm *alarm)
-{
-	struct alarm_base *base = &alarm_bases[alarm->type];
-	unsigned long flags;
-
-	spin_lock_irqsave(&base->lock, flags);
-	hrtimer_set_expires(&alarm->timer, alarm->node.expires);
-	hrtimer_restart(&alarm->timer);
-	alarmtimer_enqueue(base, alarm);
-	spin_unlock_irqrestore(&base->lock, flags);
-}
-
-/**
  * alarm_try_to_cancel - Tries to cancel an alarm timer
  * @alarm: ptr to alarm to be canceled
  *
