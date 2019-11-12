b93503bf02c329335e12395540d6bfdac4e80fe4
Kai Jones <zaksenoj@icloud.com>
2019-11-12 11:26:01 -0800

    ipv6: fix build error

--- include/linux/ipv6.h
+++ include/linux/ipv6.h
@@ -52,6 +52,7 @@ struct ipv6_devconf {
 #endif
 	__s32		disable_ipv6;
 	__s32		drop_unicast_in_l2_multicast;
+	__s32		use_oif_addrs_only;
 	__s32		accept_dad;
 	__s32		force_tllao;
 	__s32           ndisc_notify;
