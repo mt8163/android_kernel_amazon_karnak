0de6c9d243ef15ad9d9918a46398a5225599c18a
Kai Jones <zaksenoj@icloud.com>
2020-01-06 12:49:44 -0800

    drivers:md32:move md32 to /vendor/firmware instead of root because it will solve selinux issues during the build
    Change-Id: I9772afe45c5100a5c0cb050e3b724dcb3eb1045e
    Signed-off-by: Kai Jones <zaksenoj@icloud.com>

--- drivers/misc/mediatek/md32/mt8163/md32_helper.c
+++ drivers/misc/mediatek/md32/mt8163/md32_helper.c
@@ -40,8 +40,8 @@
 /* #define LOG_TO_AP_UART */
 
 #define MD32_DEVICE_NAME	"md32"
-#define MD32_DATA_IMAGE_PATH		"/md32_d.bin"
-#define MD32_PROGRAM_IMAGE_PATH		"/md32_p.bin"
+#define MD32_DATA_IMAGE_PATH		"/vendor/firmware/md32_d.bin"
+#define MD32_PROGRAM_IMAGE_PATH		"/vendor/firmware/md32_p.bin"
 
 #define MD32_SEMAPHORE		(MD32_BASE + 0x90)
 #define MD32_CLK_CTRL_BASE	(md32reg.clkctrl)
