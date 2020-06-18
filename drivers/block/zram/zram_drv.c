54fbeab80d33254867ec26ebe5937d4ce7376073
Nathan Chancellor <natechancellor@gmail.com>
2020-06-18 15:51:47 -0700

    zram: Move default compression algorithm choice to Kconfig
    Signed-off-by: Kai Jones <joneskai626@gmail.com>
    Change-Id: I0b737d9ce9b358d7402c9a54d61790c11e2de889

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -39,7 +39,7 @@
 /* Globals */
 static int zram_major;
 static struct zram *zram_devices;
-static const char *default_compressor = "lzo";
+static const char *default_compressor = CONFIG_ZRAM_DEFAULT_COMP_ALGORITHM;
 
 /* Module params (documentation at end) */
 static unsigned int num_devices = 1;
