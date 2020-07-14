e1a4d2c7249f08286d5d77851ee34236851fc8ae
brian <bphaslett@users.noreply.github.com>
2020-07-14 13:22:24 -0500

    Fixup for commit 3071f8b3e879

--- drivers/gpu/drm/i915/intel_pm.c
+++ drivers/gpu/drm/i915/intel_pm.c
@@ -6039,19 +6039,11 @@ static void i830_init_clock_gating(struct drm_device *dev)
 {
 	struct drm_i915_private *dev_priv = dev->dev_private;
 
-<<<<<<< HEAD
 	I915_WRITE(DSPCLK_GATE_D, OVRUNIT_CLOCK_GATE_DISABLE);
 
 	I915_WRITE(MEM_MODE,
 		   _MASKED_BIT_ENABLE(MEM_DISPLAY_A_TRICKLE_FEED_DISABLE) |
 		   _MASKED_BIT_ENABLE(MEM_DISPLAY_B_TRICKLE_FEED_DISABLE));
-=======
-	if (INTEL_INFO(dev)->gen < 6)
-		return;
-
-	gen6_suspend_rps(dev);
-	dev_priv->rps.enabled = false;
->>>>>>> dbea3cea6950... drm/i915: sanitize RPS resetting during GPU reset
 }
 
 void intel_init_clock_gating(struct drm_device *dev)
